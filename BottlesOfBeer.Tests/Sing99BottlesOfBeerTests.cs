e248a874817e9513029b04b16a733c36e5116a03
Victor Gomez de Juan <victorgomezdejuan@gmail.com>
2024-03-17 17:54:27 +0100

    The program is able to generate the lyrics for up to 1 bottle

--- BottlesOfBeer.Tests/Sing99BottlesOfBeerTests.cs
+++ BottlesOfBeer.Tests/Sing99BottlesOfBeerTests.cs
@@ -15,4 +15,16 @@ public class Sing99BottlesOfBeerTests
         result[0].Should().Be("No more bottles of beer on the wall, no more bottles of beer.");
         result[1].Should().Be("Go to the store and buy some more, 99 bottles of beer on the wall.");
     }
+
+    [Fact]
+    public void OneBottle()
+    {
+        var sut = new BottlesOfBeer();
+
+        var result = sut.Sing(1);
+
+        result.Length.Should().Be(4);
+        result[0].Should().Be("1 bottle of beer on the wall, 1 bottle of beer.");
+        result[1].Should().Be("Take one down and pass it around, no more bottles of beer on the wall.");
+    }
 }
--- BottlesOfBeer/99BottlesOfBeer.cs
+++ BottlesOfBeer/99BottlesOfBeer.cs
@@ -4,9 +4,20 @@ public class BottlesOfBeer
 {
     public string[] Sing(int numberOfBootles = 99)
     {
-        return new string[] {
-            "No more bottles of beer on the wall, no more bottles of beer.",
-            "Go to the store and buy some more, 99 bottles of beer on the wall." 
-        };
+        var lyrics = new List<string>();
+
+        if (numberOfBootles >= 1)
+        {
+            lyrics.Add("1 bottle of beer on the wall, 1 bottle of beer.");
+            lyrics.Add("Take one down and pass it around, no more bottles of beer on the wall.");
+        }
+
+        if (numberOfBootles >= 0)
+        {
+            lyrics.Add("No more bottles of beer on the wall, no more bottles of beer.");
+            lyrics.Add("Go to the store and buy some more, 99 bottles of beer on the wall.");
+        }
+
+        return lyrics.ToArray();
     }
 }
